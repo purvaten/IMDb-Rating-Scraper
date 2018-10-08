5de39125ef8eaeab29b96c6d2ed442878fe8a718
Purva Tendulkar <purva.tendulkar@gmail.com>
2018-10-08 11:44:11 -0400

    Made compatible with Python3

--- split.py
+++ split.py
@@ -1,13 +1,13 @@
 from bs4 import BeautifulSoup
-import urllib2
+from urllib.request import urlopen
 
 def break_words (movie_name):
-#Breaks up movie_name into constituent words	
+#Breaks up movie_name into constituent words
 	words = movie_name.split()
 	return words
 
 def print_first_word (words):
-#Returns first word of list after popping it out	
+#Returns first word of list after popping it out
 	word = words.pop(0)
 	return word
 
@@ -19,8 +19,8 @@ def print_all_words (movie_name):
 		movie_with_plus = movie_with_plus + "+" + print_first_word(words)
 	return movie_with_plus
 
-print "Enter the name of the movie"
-movie_name = raw_input(">>")
+print("Enter the name of the movie")
+movie_name = input(">>")
 movie_with_plus = print_all_words (movie_name)
 
 common_url = "http://www.imdb.com/find?ref_=nv_sr_fn&q="
@@ -28,42 +28,43 @@ common_url = "http://www.imdb.com/find?ref_=nv_sr_fn&q="
 #url to IMDb page created
 new_url =  common_url + movie_with_plus
 
-page = urllib2.urlopen(new_url)
-soup_package1 = BeautifulSoup(page)
+page = urlopen(new_url)
+soup_package1 = BeautifulSoup(page, "lxml")
 expression = soup_package1.find(class_ = "result_text")
 
-#class = "result_text" attribute not found 
+#class = "result_text" attribute not found
 if expression is None:
-	print "No results found for " + "\"" + movie_name + "\""
+	print("No results found for " + "\"" + movie_name + "\"")
 	exit(0)
 
 atag = expression.a
 movie_link = (atag['href'])
 
 #final link to IMDb page for movie created
-proper_movie_link = "http://www.imdb.com" + movie_link 
+proper_movie_link = "http://www.imdb.com" + movie_link
 
-new_page = urllib2.urlopen(proper_movie_link)
+new_page = urlopen(proper_movie_link)
 
 #URL created is invalid i.e. cannot be opened
 if new_page is None:
-	print "Proper movie link cannot be opened"
+	print("Proper movie link cannot be opened")
 	exit(0)
 
-soup_package2 = BeautifulSoup(new_page)
+soup_package2 = BeautifulSoup(new_page, "lxml")
 finding_rating = soup_package2.find("div", class_ = "star-box-details")
- 
-# class = "star-box-details" attribute does not exist 
+
+# class = "star-box-details" attribute does not exist
 if finding_rating is None:
-	print "Invalid movie name"
+	print("Invalid movie name")
 	exit(0)
 
 rating_span = finding_rating.span
 
 #Movie does not have rating
 if rating_span is None:
-	print "Type complete movie name"
+	print("Type complete movie name")
 	exit(0)
 
 #Displaying rating
 print("The rating is "+rating_span.string)
+
